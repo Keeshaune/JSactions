2177cbf6649b4ee326c8b2213fe90cd58d6eeded
Sean <millington.sean12@gmail.com>
2022-09-20 12:41:32 +0200

    finished first 2 exercises

---
+++ main.js
@@ -0,0 +1,28 @@
+//
+let string = "chicken"
+let number;
+
+// return length of a string
+//console.log(string.length)
+
+ // console.log the word train from this sentence “I dislike trains as a mode of transport”
+//  string = "I dislike trains as a mode of transport"
+// console.log(string.substring(10, 15))
+
+//Return the fifth letter of the alphabet from this string ‘abcdefghijklmnopqrstuvwxyz’
+// string = "abcdefghijklmnopqrstuvwxyz"
+// console.log(string[4])
+
+//Make this string shouty by making it all upper case ‘I love you’
+// string = "I love you";
+// console.log(string.toUpperCase())
+
+//console.log this number to 2 decimal places 1.23456789
+// number = 1.23456789
+// string = number.toString()
+// console.log(string.slice(0, 4))
+
+//Change this string into a number “45”
+string = "45"
+number = parseFloat(string)
+console.log(number)
